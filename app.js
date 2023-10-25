7d2458f3132eb7453f6356c43ea497e2f2e476c7
Mihir42 <mihir_mistry@outlook.com>
2023-10-26 00:18:35 +0100

    Added cors and added middleware

--- app.js
+++ app.js
@@ -1,81 +1,105 @@
 // Imports ---------------------------------------
 import express from "express";
-import database from './database.js';
+import database from "./database.js";
+import cors from "cors";
 
 // Configure express app -------------------------
 
 const app = new express();
 
 // Configure middleware --------------------------
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
 
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+
+  next();
+});
+
+app.use(cors({ origin: "*" }));
 // Controllers -----------------------------------
 
 const read = async (selectsql) => {
-    try {
-        const [result] = await database.query(selectsql);
-        return (result.length === 0) 
-            ? {isSuccess: false, result: null, message: 'No record(s) found'}
-            : {isSuccess: true, result: result, message: 'Records(s) successfully recovered'}
-    }
-    
-    catch(error) {
-        return {isSuccess: false, result: null, message: `Failed to execute query: ${error.message}` };
-    } 
-}
+  try {
+    const [result] = await database.query(selectsql);
+    return result.length === 0
+      ? { isSuccess: false, result: null, message: "No record(s) found" }
+      : {
+          isSuccess: true,
+          result: result,
+          message: "Records(s) successfully recovered",
+        };
+  } catch (error) {
+    return {
+      isSuccess: false,
+      result: null,
+      message: `Failed to execute query: ${error.message}`,
+    };
+  }
+};
 
 const buildDrugsselectSql = (id, variant) => {
-    let sql = '';
-    let table = 'drugs';
-    let fields = ['DrugID', 'DrugName', 'DrugDosage', 'DrugSymptoms'];
+  let sql = "";
+  let table = "drugs";
+  let fields = ["DrugID", "DrugName", "DrugDosage", "DrugSymptoms"];
 
-    switch(variant) {
-        default:
-            sql = `SELECT ${fields} from ${table}`;
-            if(id) sql += ` WHERE DrugID=${id}`;
-    }
-    return sql;
-}
+  switch (variant) {
+    default:
+      sql = `SELECT ${fields} from ${table}`;
+      if (id) sql += ` WHERE DrugID=${id}`;
+  }
+  return sql;
+};
 
 const buildPatientPrescription = (id, variant) => {
-    let sql = '';
-    let table1 = 'drugs';
-    let table2 = 'prescriptions';
-    let table3 = 'prescribeddrugs';
-    let table4 = 'patients';
-    let fields = ['drugs.DrugName', 'drugs.DrugDosage', 'drugs.DrugSymptoms', 'patients.PatientID', 'patients.PatientFirstName', 'patients.PatientLastName'];
+  let sql = "";
+  let table1 = "drugs";
+  let table2 = "prescriptions";
+  let table3 = "prescribeddrugs";
+  let table4 = "patients";
+  let fields = [
+    "drugs.DrugID",
+    "drugs.DrugName",
+    "drugs.DrugDosage",
+    "drugs.DrugSymptoms",
+    "patients.PatientID",
+    "patients.PatientFirstName",
+    "patients.PatientLastName",
+  ];
 
-    switch(variant) {
-        default:
-            sql = `SELECT ${fields} FROM ${table1}
+  switch (variant) {
+    default:
+      sql = `SELECT ${fields} FROM ${table1}
             INNER JOIN ${table2}
             ON drugs.DrugID = prescriptions.PrescriptionsDrugID
             INNER JOIN ${table3}
             ON prescriptions.PrescriptionPrescribedDrugID = prescribeddrugs.PrescribeddrugsID
             INNER JOIN ${table4}
             ON prescribeddrugs.PrescribeddrugsPatientID = patients.PatientID AND patients.PatientID = ${id};`;
-    }
-    return sql; 
-}
+  }
+  return sql;
+};
 
 const getDrugController = async (req, res, variant) => {
-    const id = req.params.id;
-    
-    // Validate request
-    // Access data 
-    const sql = buildPatientPrescription(id, variant);
-    const { isSuccess, result, message } = await read(sql);
-    if (!isSuccess) return res.status(404).json({message});
+  const id = req.params.id;
 
-    // Response to request  
-    res.status(200).json(result);
-}
+  // Validate request
+  // Access data
+  const sql = buildPatientPrescription(id, variant);
+  const { isSuccess, result, message } = await read(sql);
+  if (!isSuccess) return res.status(404).json({ message });
+
+  // Response to request
+  res.status(200).json(result);
+};
 
 // Endpoints -------------------------------------
 
-app.get('/api/drugs', (req, res) => getDrugController(req, res, null));
-app.get('/api/drugs/:id', (req, res) => getDrugController(req, res, null));
-
-
+app.get("/api/drugs", (req, res) => getDrugController(req, res, null));
+app.get("/api/drugs/:id", (req, res) => getDrugController(req, res, null));
 
 // Start server ----------------------------------
 

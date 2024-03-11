f469fbc81ec85af1bc5b33b491eece23be3bc188
Mihir42 <124581857+Mihir42@users.noreply.github.com>
2024-03-11 22:33:41 +0000

    Added diagnosis (#8)

---
+++ routers/diagnosis-Router.js
@@ -0,0 +1,26 @@
+import { Router } from "express";
+import Model from "../models/Model.js";
+import modelConfig from "../models/diagnosis-model.js";
+import database from "../database.js";
+import Accessor from "../accessor/Accessor.js";
+import Controller from "../controller/Controller.js";
+
+// Model -----------------------------------------------
+
+const model = new Model(modelConfig);
+
+// Data accessors --------------------------------------
+
+const accessor = new Accessor(model, database);
+
+// Controllers ----------------------------------------
+
+const controller = new Controller(accessor);
+
+// Endpoints ------------------------------------------
+
+const router = Router();
+router.get("/", (req, res) => controller.get(req, res, null));
+router.get("/:id", (req, res) => controller.get(req, res, null));
+
+export default router;
